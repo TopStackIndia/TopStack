71107655624d4b52fee2943d84cb13c4e1af1371
Rajdeep Das <rajdeepdas.india@gmail.com>
2017-08-22 23:40:30 +0530

    fix home redirect from team page

--- app/team.php
+++ app/team.php
@@ -87,7 +87,7 @@ if (!$user_home->is_logged_in()) {
                         <!-- Collect the nav links, forms, and other content for toggling -->
                         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                             <ul class="nav navbar-nav navbar-right">
-                                <li><a href="index.php#slider">Home</a>
+                                <li><a href="index.php">Home</a>
                                 </li>
                                 <li><a href="index.php#about">about</a>
                                 </li>
